e7876212160a8d5909c10e9113aad59dd8c54a68
adelin.carabas <adelinromana@gmail.com>
2025-03-07 01:36:26 +0200

    initial setup

---
+++ backend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Npgsql;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = File.ReadAllText("dbcredentials.txt");
+
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        string sql = "SELECT * FROM public.\"testTable\"";
+        using var command = new NpgsqlCommand(sql, connection);
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            Console.WriteLine($"ID: {id}");
+        }
+    }
+}
